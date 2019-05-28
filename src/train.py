86bccba6bcf1bee612f92ce31169f79d47437170
Arsalan Cheema <arsalanc.v2@gmail.com>
2019-05-28 23:33:32 +0800

    add selected model parameters as arguments

--- src/train.py
+++ src/train.py
@@ -17,28 +17,29 @@ def load_data(path):
 	data_clas = TextClasDataBunch.from_df(train_df=train_df, valid_df=val_df, test_df=test_df, vocab=data_lm.train_ds.vocab, bs=32, text_cols=args.textcolumn, label_cols=args.labelcolumn, path="")
 	return data_lm, data_clas
 
-def train(data_lm, data_clas, lm_epochs=1, tc_epochs=1):
+def train(data_lm, data_clas, lm_epochs=1, tc_epochs=1, drop_mult=1, lr=1e-2):
 	# create language model with pretrained weights
-	learn = language_model_learner(data_lm, arch=AWD_LSTM, drop_mult=0.5)
+	learn = language_model_learner(data_lm, arch=AWD_LSTM, drop_mult=drop_mult)
 	# train language model using one cycle policy
-	learn.fit_one_cycle(lm_epochs, 1e-2)
+	learn.fit_one_cycle(lm_epochs, lr)
 	learn.save_encoder(LANGUAGE_MODEL_NAME)
-	learn = text_classifier_learner(data_clas, arch=AWD_LSTM, drop_mult=0.5)
+	learn = text_classifier_learner(data_clas, arch=AWD_LSTM, drop_mult=drop_mult)
 	learn.load_encoder(LANGUAGE_MODEL_NAME)
-	learn.fit_one_cycle(tc_epochs, 1e-2)
+	learn.fit_one_cycle(tc_epochs, lr)
 	return learn
 
 def show_results(learn):
 	preds, y, losses = learn.get_preds(DatasetType.Train, with_loss=True)
 	interp = ClassificationInterpretation(learn, preds, y, losses)
 	print(interp.confusion_matrix(slice_size=10))
-	print(learn.show_results())
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-dp', '--datasetpath', help='str: path to csv dataset', type=str, action='store', required=True)
 	parser.add_argument('-tcol', '--textcolumn', help='int: column index of text', type=int, action='store', required=True)
 	parser.add_argument('-lcol', '--labelcolumn', help='int: column index of labels', type=int, action='store', required=True)
+	parser.add_argument('-dm', '--dropmult', help='float: amount to scale dropout values by', type=float, action='store', default=1)
+	parser.add_argument('-lr', '--learningrate', help='float: learning rate for both language and text models', type=float, action='store', default=1e-2)
 	parser.add_argument('-lme', '--languagemodelepochs', help='int: number of epochs to train the language model learner', type=int, action='store', default=1)
 	parser.add_argument('-tce', '--textclassifierepochs', help='int: number of epochs to train the text classifier', type=int, action='store', default=1)
 	args = parser.parse_args()
@@ -48,6 +49,6 @@ if __name__ == '__main__':
 	TEXT_MODEL_NAME = 'textclassifier.pkl'
 
 	data_lm, data_clas = load_data(args.datasetpath)
-	learn = train(data_lm, data_clas, args.languagemodelepochs, args.textclassifierepochs)
+	learn = train(data_lm, data_clas, args.languagemodelepochs, args.textclassifierepochs, args.dropmult, args.learningrate)
 	learn.export('models/' + TEXT_MODEL_NAME)
 	show_results(learn)
