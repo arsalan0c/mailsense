f48e2e9194a76f863b1b9784c682231bd7740e8d
Arsalan Cheema <arsalanc.v2@gmail.com>
2019-07-14 10:32:35 +0800

    refactor to separate argument processing and inference functionality

--- src/inference.py
+++ src/inference.py
@@ -1,15 +1,42 @@
 #!/usr/bin/env python3
 
-import argparse
+# This script is used to perform inference to classify a text using a trained Fast.ai language model.
+
 from fastai import *
 from fastai.text import *
 
-parser = argparse.ArgumentParser(description='process arguments required for performing inference with trained model on input text')
-parser.add_argument('-t', '--text', help='string: text to perform inference on', type=str, action='store', required=True)
-args = parser.parse_args()
+import argparse
 
-MODEL_DIR = 'models'
-MODEL_NAME = 'textclassifier_encoder'
+def initialize_model(model_dir, model_name):
+	'''Initializes a saved Fast.ai language model.
 
-learn = load_learner(MODEL_DIR, MODEL_NAME)
-result = learn.predict(args.text)
+	Args:
+		model_dir: Directory path for a Fast.ai language model.
+		model_name: File name for a Fast.ai language model.
+	'''
+	# prevent exposing the learner
+	global learn
+	learn = load_learner(model_dir, model_name)
+
+def predict(text):
+	'''Returns the polarity label after performing inference on a text.
+
+	Uses a trained Fast.ai language model.
+
+	Args:
+		text: Text to perform inference on.
+	'''
+	preds = learn.predict(text)
+	polarity = str(preds[0])
+	return polarity
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='process arguments required for performing inference with trained model on input text')
+	parser.add_argument('-md', '--modeldirectory', help='string: path to directory containing model file', type=str, action='store', required=True)
+	parser.add_argument('-mn', '--modelname', help='string: name of model file', type=str, action='store', required=True)
+	parser.add_argument('-t', '--text', help='string: text to perform inference on', type=str, action='store', required=True)
+	args = parser.parse_args()
+
+	initialize_model(args.modeldirectory, args.modelname)
+	polarity = predict(args.text)
+	print(polarity)
